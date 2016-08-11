aaadd4e2d3c08efe2deb2d0afb43d088fca5dba9
Rajat Vij <rajatvij@users.noreply.github.com>
2016-08-11 18:27:36 -0400

    Update 5_optimized.py

--- 5_optimized.py
+++ 5_optimized.py
@@ -11,29 +11,39 @@ numbers from 1 to 20?
 
 def smallest_product(n):
     factors = []
-
-    if n % 2 == 0:
+    # Check if the number is greater than 2.
+    if n > 3:
         factors.append(2)
+        # iterate through all odd numbers.
+        for y in range(3, n, 2):
+            # Check and add prime numbers to the factors.
+            if is_prime(y):
+                factors.append(y)
+        # Check and add all the squares, cubes or other powered numbers 
+        # less than the number
+        for x in range(2,n):
+            # Square root of a number is the largest power 2 can have 
+            # which is smaller than number itself.
+            for y in range(2,int(n**0.5)):
+                if x**y < n:
+                    factors.append(x**y)
+                    # Check and remove the factors or powered number.
+                    if x in factors:
+                        factors.remove(x)
 
-    for y in range(3, n, 2):
-        if is_prime(y):
-            factors.append(y)
+        # Check and remove any number is divisible by other number in list.
+        for item1 in factors:
+            for item2 in factors:
+                if item1 > item2 and item1 % item2 ==0:
+                    factors.remove(item2)
 
-    for x in range(2,n):
-        for y in range(2,int(n**0.5)):
-            if x**y < n:
-                factors.append(x**y)
-                if x in factors:
-                    factors.remove(x)
-
-    for item1 in factors:
-        for item2 in factors:
-            if item1 > item2 and item1 % item2 ==0:
-                factors.remove(item2)
-
-    smallest_product = 1
-    for item in factors:
-        smallest_product = smallest_product * item
+        smallest_product = 1
+        # Get the product of all numbers in list.
+        for item in factors:
+            smallest_product = smallest_product * item
+    
+    else:
+        return "Please enter a larger number"
 
     return smallest_product
 
